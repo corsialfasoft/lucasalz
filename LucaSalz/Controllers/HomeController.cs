75301e4a4f83005066b6c2195b3580d12ddb7738
lucasalzone <luca.salzone@gmail.com>
2018-04-05 12:33:20 +0200

    init project

---
+++ LucaSalz/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LucaSalz.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            ViewBag.MioContenuto ="sono in index controller";
+            return View();
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.MioContenuto ="esempio";
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
